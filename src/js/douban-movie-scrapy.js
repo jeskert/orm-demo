a4a53931031d9c77f33bfaf674795002f0da086b
jeskert <568361463@qq.com>
2017-08-02 11:33:09 +0800

    orm101

---
+++ src/js/douban-movie-scrapy.js
@@ -0,0 +1,88 @@
+const request = require('sync-request');
+
+function getDoubanTop250Movies(start, count) {
+    let res = request('GET', `https://api.douban.com/v2/movie/top250?start=${start}&count=${count}`);
+    return JSON.parse(res.getBody()).subjects;
+}
+
+function getDoubanMovieByTag(start, count, tag) {
+    let res = request('GET', `https://api.douban.com/v2/movie/search?tag=${tag}&start=${start}&count=${count}`);
+    return JSON.parse(res.getBody()).subjects;
+}
+
+function createMovieGenres(movieGenres, req) {
+    for (let movieGenre of movieGenres) {
+        req.models.MovieGenre.create(movieGenre, function (err, result) {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log("success insert genre")
+            }
+        });
+    }
+}
+function createMovie(req, movieToSave) {
+    req.models.Movie.create(movieToSave, function (err, result) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("success insert movie")
+        }
+    });
+}
+function saveSelectedData(moviesfromDouban, genres, req) {
+    for (let movie of moviesfromDouban) {
+        let casts = [], directors = [];
+        for (let cast of movie.casts) {
+            casts.push(cast.name);
+        }
+        for (let director of movie.directors) {
+            directors.push(director.name);
+        }
+        let movieToSave = {
+            id: movie.id,
+            alt: movie.alt,
+            year: movie.year,
+            title: movie.title,
+            rating: movie.rating.average,
+            original_title: movie.original_title,
+            directors: directors.join(","),
+            casts: casts.join(","),
+            image: movie.images.medium
+        };
+
+        let movieGenres = [];
+        for (let currentGenre of movie.genres) {
+            for (let genre of genres) {
+                if (currentGenre == genre.name) {
+                    movieGenres.push({
+                        movie_id: movie.id,
+                        genre_id: genre.id
+                    });
+                    break;
+                }
+            }
+        }
+
+        createMovie(req, movieToSave);
+        createMovieGenres(movieGenres, req);
+    }
+}
+function saveTop250Data(req, genres) {
+    let moviesfromDouban = getDoubanTop250Movies(0,100).concat(getDoubanTop250Movies(100,100)).concat(getDoubanTop250Movies(200, 100));
+    saveSelectedData(moviesfromDouban, genres, req);
+}
+
+function saveDataByTag(req, tag, genres) {
+    let moviesFromDouban = [];
+    for (let start = 0; start < 20; start += 20) {
+        moviesFromDouban.concat(getDoubanMovieByTag(start, 20, tag.name));
+    }
+    saveSelectedData(moviesFromDouban, genres, req);
+}
+
+
+module.exports = {
+    saveTop250Data,
+    saveDataByTag
+};
--- src/js/ormTest.js
+++ src/js/ormTest.js
@@ -1,33 +1,68 @@
 const orm = require('orm');
 const express = require('express');
-const scrapy = require('./douban-movie-scrapy')
+const scrapy = require('./douban-movie-scrapy');
 let app = express();
 
-app.use(orm.express("mysql://root:@localhost:3306/test", {
+app.use(orm.express("sqlite:///Users/wyli/WebstormProjects/movies.db", {
     define: function (db, models, next) {
         // 定义model 把数据库的类型映射为node.js的类型，长度、唯一、是否可空等配置将使用数据库中的定义，
         // Node.js的类型有Number、String、Boolean、Date、Object、Buffer和Array。
-        models.User = db.define("user", {
+        models.Genre = db.define("genre", {
             id : Number,
-            name : String,
-            age : Number
+            name : String
+        });
+
+        models.Movie = db.define("movie", {
+            id: Number,
+            alt : String,
+            year : Number,
+            title : String,
+            rating : String,
+            original_title: String,
+            directors : String,
+            casts : String,
+            image: String
+        });
+
+        models.MovieGenre = db.define("movie_genre", {
+            id : Number,
+            movie_id : Number,
+            genre_id : Number
         });
         next();
     }
 }));
 
 app.get('/', function (req, res, next) {
-    req.models.User.find(function (err, users) {
+    req.models.Movie.find(function (err, movies) {
         if (err) {
             res.send(err);
         }
 
-        res.send(JSON.stringify(users));
-    })
+        res.send(JSON.stringify(movies));
+    });
 });
 
 app.get('/scrapy', function (req, res, next) {
-    scrapy();
+    req.models.Genre.find(function (err, genres) {
+        if (err) {
+            res.send(err);
+        }
+        res.send(scrapy.saveTop250Data(req, genres));
+    })
+});
+
+
+app.get('/scrapyByTag', function (req, res, next) {
+    req.models.Genre.find(function (err, genres) {
+        if (err) {
+            res.send(err);
+        }
+        for (let tag of genres) {
+            scrapy.saveDataByTag(req, tag, genres);
+        }
+        res.send('success');
+    })
 });
 
 var server = app.listen(8081, function () {
